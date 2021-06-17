6bb59214f648272eb4e4f39277def76e4874f367
Jan-Gabriel Mylius <jgmylius@posteo.de>
2021-06-17 22:00:29 +0200

    added statistics on unsolved questions for each annotator.

--- statistics.py
+++ statistics.py
@@ -4,6 +4,8 @@ import pandas as pd
 from os.path import isfile
 import plotly.express as px
 from sklearn.metrics import classification_report
+import argparse
+
 
 def annodata_to_df():
     """
@@ -132,7 +134,8 @@ def plot_ref_balance(plot=True):
         fig_balance.show()
     return reference["is_bicycle"].value_counts()
 
-def calc_f1(df,plot = True):
+
+def calc_f1(df, plot=True):
     """
     Takes the annotations DataFrame and returns a Dataframe containing a classification report for each annotator.
     If plot is the f1 score will be plotted.
@@ -143,37 +146,125 @@ def calc_f1(df,plot = True):
     plot: A Boolean
     """
     reference = pd.read_json(r"./data/references.json").T
-    answer_df = df.xs("answer", level=1, axis=1, drop_level=False).droplevel(1,axis = 1)
+    answer_df = df.xs("answer", level=1, axis=1, drop_level=False).droplevel(1, axis=1)
     classification = {}
     for annotator in answer_df.columns:
-        ref_array = reference["is_bicycle"][answer_df[annotator].dropna().index].sort_index().values
-        annot_array = answer_df[annotator].dropna().sort_index().map(dict(yes=True, no=False)).values
-        #Weirdly annot_array sometimes contains bools instead of np.bool_s
+        ref_array = (
+            reference["is_bicycle"][answer_df[annotator].dropna().index]
+            .sort_index()
+            .values
+        )
+        annot_array = (
+            answer_df[annotator]
+            .dropna()
+            .sort_index()
+            .map(dict(yes=True, no=False))
+            .values
+        )
+        # Weirdly annot_array sometimes contains bools instead of np.bool_s
         if type(annot_array[0]) == bool:
             annot_array = [np.bool_(i) for i in annot_array]
-        classification[annotator] = pd.DataFrame(classification_report(ref_array,annot_array, output_dict=True))
-    class_df = pd.concat(classification,axis = 1)
+        classification[annotator] = pd.DataFrame(
+            classification_report(ref_array, annot_array, output_dict=True)
+        )
+    class_df = pd.concat(classification, axis=1)
     if plot:
-        class_df_plot = class_df.xs("accuracy", level=1, axis=1, drop_level=False).iloc[0].T.droplevel(1)
+        class_df_plot = (
+            class_df.xs("accuracy", level=1, axis=1, drop_level=False)
+            .iloc[0]
+            .T.droplevel(1)
+        )
+        print(class_df_plot)
         fig_dur = px.bar(
             class_df_plot,
-            y=["f1-score"],
-            barmode="group",
             title="F1-Score of the Accuracy",
         )
-        fig_dur.update_yaxes(title_text="Time[s]")
+        fig_dur.update_yaxes(title_text="f1-score")
         fig_dur.show()
     return class_df
 
+
+def get_unsolved(df, plot=True):
+    """
+    Takes the annotations DataFrame and returns a Dataframe containing the percentage of can't solve answers for each annotator.
+    If plot is True a plot of the data will be created.
+
+    Arguments
+    ----------
+    df: The annotations DataFrame created by annodata_to_df()
+    plot: A Boolean
+    """
+    #cant_solve
+    cant_solve = df.xs("cant_solve", level=1, axis=1, drop_level=False).droplevel(
+        1, axis=1
+    )
+    cant_solve_dict = {}
+    for annotator in df.xs("answer", level=1, axis=1, drop_level=False).droplevel(
+        1, axis=1
+    ):
+        cant_solve_dict[annotator] = cant_solve[annotator].value_counts()
+    cant_solve = pd.concat(cant_solve_dict, axis=1)
+    cant_solve = pd.DataFrame(
+        cant_solve.fillna(0).iloc[1] * 100 / cant_solve.iloc[0], columns=['% "can\'t solves"']
+    )
+    #corrupt
+    corrupt_annot = df.xs("corrupt_data", level=1, axis=1, drop_level=False).droplevel(
+        1, axis=1
+    )
+    corrupt_annot_dict = {}
+    for annotator in df.xs("answer", level=1, axis=1, drop_level=False).droplevel(
+        1, axis=1
+    ):
+        corrupt_annot_dict[annotator] = corrupt_annot[annotator].value_counts()
+    corrupt_annot = pd.concat(corrupt_annot_dict, axis=1)
+    corrupt_annot = pd.DataFrame(
+        corrupt_annot.fillna(0).iloc[1] * 100 / corrupt_annot.iloc[0],
+        columns=['% "corrupt"'],
+    )
+    unsolved_df = pd.concat([corrupt_annot,cant_solve])
+    if plot:
+        fig_dur = px.bar(
+            unsolved_df,
+            y=['% "can\'t solves"','% "corrupt"'],
+            barmode="group",
+            title="Percentage of Unsolved Questions",
+        )
+        fig_dur.update_yaxes(title_text="%")
+        fig_dur.show()
+    return cant_solve
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Statistics on Annotated Data"
+    )
+    parser.add_argument(
+        "-p",
+        action="store_true",
+        help="If set the data will be plotted.",
+    )
+    plot = parser.parse_args().p
     try:
         df = pd.read_pickle("./.dataframe.pkl")
     except:
         df = annodata_to_df()
         df.to_pickle("./.dataframe.pkl")
-    num_annotators = df.xs("answer", level=1, axis=1, drop_level=False).droplevel(1,axis=1).shape[1]
-    num_annotations = {}
-    get_duration_stat(df,plot=False)
-    get_num_of_annot_stat(df, plot=False)
-    plot_ref_balance(plot=False)
-    print(calc_f1(df,plot=False))
+    #get_corrupt_annot(df)
+    
+    num_annotators = (
+        df.xs("answer", level=1, axis=1, drop_level=False).droplevel(1, axis=1).shape[1]
+    )
+    #1a
+    print(num_annotators)
+    #1b
+    print(get_duration_stat(df,plot))
+    #1c
+    print(get_num_of_annot_stat(df,plot))
+    #1d
+    #2a
+    print(get_unsolved(df,plot))
+    #3
+    print(plot_ref_balance(plot))
+    #4
+    print(calc_f1(df,plot))
+
